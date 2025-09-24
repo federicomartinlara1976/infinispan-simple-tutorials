5488ef843ef2a8e1bdb78cead103bb73cfd150dd
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-09-24 16:06:34 +0200

    Ajustar log y lambda

--- integrations/spring-boot/cache-remote-reactor/src/main/java/org/infinispan/tutorial/simple/spring/remote/BasqueNamesCachingApp.java
+++ integrations/spring-boot/cache-remote-reactor/src/main/java/org/infinispan/tutorial/simple/spring/remote/BasqueNamesCachingApp.java
@@ -1,42 +1,41 @@
 package org.infinispan.tutorial.simple.spring.remote;
 
 import org.infinispan.tutorial.simple.connect.TutorialsConnectorHelper;
-import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
+@Slf4j
 public class BasqueNamesCachingApp {
 
-   private static ConfigurableApplicationContext ctx;
+	private static ConfigurableApplicationContext ctx;
 
-   public static void main(String... args) {
-      TutorialsConnectorHelper.startInfinispanContainer();
-      if (TutorialsConnectorHelper.isContainerStarted()) {
-         String[] actualArgs = new String[]{"infinispan.remote.server-list=127.0.0.1:" + TutorialsConnectorHelper.INFINISPAN_CONTAINER.getFirstMappedPort()};
-         ctx = SpringApplication.run(BasqueNamesCachingApp.class, actualArgs);
-      } else {
-         ctx = SpringApplication.run(BasqueNamesCachingApp.class, args);
-      }
-   }
+	public static void main(String... args) {
+		TutorialsConnectorHelper.startInfinispanContainer();
+		if (TutorialsConnectorHelper.isContainerStarted()) {
+			String[] actualArgs = new String[] { "infinispan.remote.server-list=127.0.0.1:"
+					+ TutorialsConnectorHelper.INFINISPAN_CONTAINER.getFirstMappedPort() };
+			ctx = SpringApplication.run(BasqueNamesCachingApp.class, actualArgs);
+		} else {
+			ctx = SpringApplication.run(BasqueNamesCachingApp.class, args);
+		}
+	}
 
-   public static void exitApplication() {
-      int staticExitCode = SpringApplication.exit(ctx, new ExitCodeGenerator() {
+	public static void exitApplication() {
+		int staticExitCode = SpringApplication.exit(ctx, () -> {
+			TutorialsConnectorHelper.stopInfinispanContainer();
+			// no errors
+			return 0;
+		});
 
-         @Override
-         public int getExitCode() {
-            TutorialsConnectorHelper.stopInfinispanContainer();
-            // no errors
-            return 0;
-         }
-      });
-
-      System.out.println("Exit");
-      System.exit(staticExitCode);
-   }
+		log.info("Exit");
+		System.exit(staticExitCode);
+	}
 }
